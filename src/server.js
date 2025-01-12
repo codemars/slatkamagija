5b5dbea4397ed5ef0289f47fb65949cb66881d7e
Marko Đokić <160076395+codemars@users.noreply.github.com>
2025-01-12 23:12:44 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,8 +12,8 @@ app.listen(5000, () => console.log("Server Running"));
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: "markojh13@gmail.com",
-      pass: "aaem fjze orzg fudb",
+      user: process.env.REACT_APP_USER,
+      pass:process.env.REACT_APP_PASSWORD,
     },
   });
   
