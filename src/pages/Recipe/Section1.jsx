3df7b332d33ccd5024a91d2d304b8b2361566bbe
codemars <markosbb13@gmail.com>
2026-03-04 21:40:05 +0100

    fix

--- src/pages/Recipe/Section1.jsx
+++ src/pages/Recipe/Section1.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import palacinke from "../../assets/recipe/americke.webp";
